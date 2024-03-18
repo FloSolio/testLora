eb874dd74d370c5dc05ba675fc5b17e2b926839c
FloSolio <flebrun@solioti.fr>
2024-03-18 14:04:36 +0100

    premier comit

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "sx1302_hal/libloragw/inc/loragw_hal.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Hello Solioti 2!\n");
+
+    printf("%s",lgw_version_info() );
+    return 0;
+}
